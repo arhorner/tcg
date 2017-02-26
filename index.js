18875a2bb3e141db308a3ebaa24534966ea5a907
Luke Barratt <lukebarratt@Lukes-MacBook-Pro.local>
2017-02-26 22:52:25 +0000

    added code to fetch pokemon card data, rendered out data to html, styled with css

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
+const request = require('superagent');
+
+app.use('/assets', express.static('app/assets'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'app/templates');
+
+app.get('/browser', (req, res) => {
+  res.render('layout', {
+    title: 'TCG Browser'
+  });
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello TCG');
+  request
+  .get('https://api.pokemontcg.io/v1/cards')
+  .end((error, data) => {
+    const { cards } = data.body;
+
+    const pageData = Object.assign({
+      title: 'TCG Browser',
+      cards
+    });
+
+    res.render('layout', pageData);
+  });
 });
 
 if (process.env.NODE_ENV != 'test') {
